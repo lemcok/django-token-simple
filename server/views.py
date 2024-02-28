845ccd4eb6a201f68e5d5e14ec9ae114315231d1
Ricardo Flores <rik.fm1809@gmail.com>
2024-02-28 01:12:19 -0500

    agregar la function login

--- server/views.py
+++ server/views.py
@@ -5,10 +5,17 @@ from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404 ## Busca un objeto en la base de datos sino lo encuentra da un error 404
 
 @api_view(['POST'])
 def login(request):
-  return Response({})
+  user = get_object_or_404(User, username=request.data['username']) ## El user es un diccionario de python
+  if not user.check_password(request.data["password"]):
+    return Response({"error": "Invalid password"}, status=status.HTTP_400_BAD_REQUEST)
+  
+  token, created = Token.objects.get_or_create(user=user)
+  serializer = UserSerializer(instance=user) ## Pasamos de un diccionario a un objeto json el user
+  return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
@@ -25,7 +32,7 @@ def register(request):
     token = Token.objects.create(user=user)
     return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
 
-  return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
